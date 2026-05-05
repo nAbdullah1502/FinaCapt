6e3f1860e22140cae3c7d22d18f30bef594bb634
Medicode <ognennoye@gmail.com>
2026-05-05 21:28:08 +0500

    IncomeDAO.kt created

---
+++ app/src/main/java/com/example/finacapt/data/dao/IncomeDAO.kt
@@ -0,0 +1,22 @@
+package com.example.finacapt.data.dao
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.finacapt.data.entity.Income
+import kotlinx.coroutines.flow.Flow
+
+
+interface IncomeDAO {
+    @Upsert
+    suspend fun insertIncome(income: Income)
+
+    @Delete
+    suspend fun deleteIncome(income: Income)
+
+    @Query("SELECT * FROM income ORDER BY timestamp DESC")
+    fun getAllIncome(): Flow<List<Income>>
+
+    @Query("SELECT SUM(amount) FROM income")
+    fun getTotalIncome(): Flow<Double?>
+}
