66b67af5adae01a83c038bc57df355bd28a0b3d4
Medicode <ognennoye@gmail.com>
2026-05-05 21:06:46 +0500

    expense entity created & defined foreignKey to the category entity

---
+++ app/src/main/java/com/example/finacapt/data/entity/Expense.kt
@@ -0,0 +1,29 @@
+package com.example.finacapt.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("category_id"),
+            onUpdate = CASCADE,
+            onDelete = CASCADE
+        ),
+    ],
+    indices = [Index("category_id")]
+)
+data class Expense(
+    val name: String,
+    @ColumnInfo(name="category_id")
+    val categoryId: Int,
+    val timestamp: Long,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
