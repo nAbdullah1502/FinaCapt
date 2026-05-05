872c35c3b4b93e494c787c7cc2a1f996f0d9086f
Medicode <ognennoye@gmail.com>
2026-05-05 21:07:48 +0500

    database created

--- app/src/main/java/com/example/finacapt/data/Database.kt
+++ app/src/main/java/com/example/finacapt/data/Database.kt
@@ -1,4 +1,26 @@
 package com.example.finacapt.data
 
-class Database {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.finacapt.data.dao.CategoryDAO
+import com.example.finacapt.data.dao.ExpenseDAO
+import com.example.finacapt.data.dao.IncomeDAO
+import com.example.finacapt.data.entity.Category
+import com.example.finacapt.data.entity.Expense
+import com.example.finacapt.data.entity.Income
+
+
+@Database(
+    entities = [
+        Category::class,
+        Income::class,
+        Expense::class
+    ],
+    version = 1,
+    exportSchema = false,
+)
+abstract class Database: RoomDatabase() {
+    abstract fun categoryDao(): CategoryDAO
+    abstract fun incomeDao(): IncomeDAO
+    abstract fun expenseDao(): ExpenseDAO
 }
