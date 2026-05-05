04ace27cc70ecd1805475ce75c4d0448c48f5d93
Medicode <ognennoye@gmail.com>
2026-05-05 21:08:13 +0500

    basic CategoryDAO created

---
+++ app/src/main/java/com/example/finacapt/data/dao/CategoryDAO.kt
@@ -0,0 +1,23 @@
+package com.example.finacapt.data.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.finacapt.data.entity.Category
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface CategoryDAO {
+    @Upsert
+    suspend fun upsertCategory(category: Category)
+
+    @Query("SELECT * FROM category")
+    fun getAllCategories(): Flow<List<Category>>
+
+    @Query("SELECT * FROM category WHERE id = :id")
+    suspend fun getCategoryById(id: Int): Category?
+
+    @Query("DELETE FROM category WHERE title = :title")
+    suspend fun deleteByTitle(title:String)
+}
