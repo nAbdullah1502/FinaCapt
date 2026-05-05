e3c133ff56a7ffb0bd1a5b056d712ecb39d66047
Medicode <ognennoye@gmail.com>
2026-05-05 21:29:07 +0500

    ExpenseDAO.kt created

---
+++ app/src/main/java/com/example/finacapt/data/dao/ExpenseDAO.kt
@@ -0,0 +1,27 @@
+package com.example.finacapt.data.dao
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.finacapt.data.entity.Expense
+import kotlinx.coroutines.flow.Flow
+
+interface ExpenseDAO {
+    @Upsert
+    suspend fun upsertExpense(expense: Expense)
+
+    @Delete
+    suspend fun deleteExpense(expense: Expense)
+
+    @Query("SELECT * FROM expense ORDER BY timestamp DESC")
+    fun getAllExpenses(): Flow<List<Expense>>
+
+    @Query("SELECT * FROM expense WHERE categoryId = :categoryId ORDER BY timestamp DESC")
+    fun getExpensesByCategory(categoryId: Int): Flow<List<Expense>>
+
+    @Query("SELECT SUM(amount) FROM expense WHERE categoryId = :categoryId")
+    fun getTotalByCategory(categoryId: Int): Flow<Double?>
+
+    @Query("SELECT SUM(amount) FROM expense")
+    fun getTotalExpenses(): Flow<Double?>
+}
