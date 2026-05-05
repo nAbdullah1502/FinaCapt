c2e46041c65db8f8347347370f4cb4ee395a8116
Medicode <ognennoye@gmail.com>
2026-05-05 21:35:27 +0500

    repo interface with all future use cases

---
+++ app/src/main/java/com/example/finacapt/domain/repository/Repository.kt
@@ -0,0 +1,26 @@
+package com.example.finacapt.domain.repository
+
+import com.example.finacapt.data.entity.Category
+import com.example.finacapt.data.entity.Expense
+import com.example.finacapt.data.entity.Income
+import kotlinx.coroutines.flow.Flow
+
+interface Repository{
+    suspend fun upsertCategory(category: Category)
+    fun getAllCategories(): Flow<List<Category>>
+    suspend fun getCategoryById(id: Int): Category?
+    suspend fun deleteByTitle(title:String)
+
+    suspend fun upsertExpense(expense: Expense)
+    suspend fun deleteExpense(expense: Expense)
+    fun getAllExpenses(): Flow<List<Expense>>
+    fun getExpensesByCategory(categoryId: Int): Flow<List<Expense>>
+    fun getTotalByCategory(categoryId: Int): Flow<Double?>
+    fun getTotalExpenses(): Flow<Double?>
+
+    suspend fun insertIncome(income: Income)
+    suspend fun deleteIncome(income: Income)
+    fun getAllIncome(): Flow<List<Income>>
+    fun getTotalIncome(): Flow<Double?>
+}
+
