1398ac75a5a7e02a290a0271f760a103328658c3
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-10 03:08:08 +0500

    created CategoryEvent.kt

---
+++ app/src/main/java/com/example/finacapt/presentation/categories/CategoryEvent.kt
@@ -0,0 +1,10 @@
+package com.example.finacapt.presentation.categories
+
+import com.example.finacapt.data.entity.Category
+
+sealed class CategoryEvent {
+    data class DeleteCategory(val category: Category) : CategoryEvent()
+    data object UpsertCategory : CategoryEvent()
+    data class SetCategoryTitle (val setTitle: String): CategoryEvent()
+    data class SetCategoryLimit (val setLimit: Double): CategoryEvent()
+}
