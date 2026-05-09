b1df0a205c677481e6267fab63263d028846218e
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-10 03:07:18 +0500

    wrapper around use cases

---
+++ app/src/main/java/com/example/finacapt/domain/usecase/CategoryUseCases.kt
@@ -0,0 +1,8 @@
+package com.example.finacapt.domain.usecase
+
+import javax.inject.Inject
+
+data class CategoryUseCases @Inject constructor(
+    val getAllCategories: GetAllCategories,
+    val upsertCategory: UpsertCategory
+)
