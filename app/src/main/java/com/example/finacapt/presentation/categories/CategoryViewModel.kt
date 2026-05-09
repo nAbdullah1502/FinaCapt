2b347a4097ee718c73bc2ccd093b20fbcf226103
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-10 03:08:24 +0500

    created CategoryViewModel.kt

---
+++ app/src/main/java/com/example/finacapt/presentation/categories/CategoryViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.finacapt.presentation.categories
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finacapt.data.entity.Category
+import com.example.finacapt.domain.usecase.CategoryUseCases
+import com.example.finacapt.domain.usecase.UpsertCategory
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryViewModel @Inject constructor(
+    val useCase: UpsertCategory
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CategoryState())
+    val state = _state.asStateFlow()
+
+    fun onEvent(event: CategoryEvent){
+        when(event){
+            is CategoryEvent.SetCategoryTitle -> _state.update {it.copy(stateTitle=event.setTitle)}
+            is CategoryEvent.SetCategoryLimit -> _state.update {it.copy(stateLimit=event.setLimit)}
+            is CategoryEvent.DeleteCategory -> TODO()
+
+            is CategoryEvent.UpsertCategory -> {
+                val title = state.value.stateTitle
+                val limit = state.value.stateLimit
+                if (title.isBlank()) { return }
+                val category = Category(title = title, limit = limit)
+                viewModelScope.launch {
+                    val useCase = null
+                    useCase?.invoke(category)
+                }
+            }
+
+        }
+    }
+}
