500fd73095ab81c4c2b5bd28032c38145cee7e46
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-10 03:07:56 +0500

    created CategoryState.kt

---
+++ app/src/main/java/com/example/finacapt/presentation/categories/CategoryState.kt
@@ -0,0 +1,9 @@
+package com.example.finacapt.presentation.categories
+
+import com.example.finacapt.data.entity.Category
+
+data class CategoryState(
+    val categoriesList : List<Category> = emptyList(),
+    val stateTitle : String = "",
+    val stateLimit : Double = 0.0
+)
