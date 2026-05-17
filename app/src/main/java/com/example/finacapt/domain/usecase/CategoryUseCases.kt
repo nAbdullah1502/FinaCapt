c02a7e79d3207d9b50d4120b912229c6485523a4
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-17 09:41:03 +0500

    DeleteCategoryByTitle: use case

--- app/src/main/java/com/example/finacapt/domain/usecase/CategoryUseCases.kt
+++ app/src/main/java/com/example/finacapt/domain/usecase/CategoryUseCases.kt
@@ -4,5 +4,6 @@ import javax.inject.Inject
 
 data class CategoryUseCases @Inject constructor(
     val getAllCategories: GetAllCategories,
-    val upsertCategory: UpsertCategory
+    val upsertCategory: UpsertCategory,
+    val deleteCategoryByTitle: DeleteCategoryByTitle,
 )
