f87ef85a1abb495e9b24ad7eb951895abc54a27a
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-17 09:18:03 +0500

    di: providesRepository and providesCategoryUseCases added to the AppModule

--- app/src/main/java/com/example/finacapt/di/AppModule.kt
+++ app/src/main/java/com/example/finacapt/di/AppModule.kt
@@ -3,6 +3,11 @@ package com.example.finacapt.di
 import android.app.Application
 import androidx.room.Room
 import com.example.finacapt.data.Database
+import com.example.finacapt.data.repository.RepositoryImpl
+import com.example.finacapt.domain.repository.Repository
+import com.example.finacapt.domain.usecase.CategoryUseCases
+import com.example.finacapt.domain.usecase.GetAllCategories
+import com.example.finacapt.domain.usecase.UpsertCategory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,5 +24,23 @@ object DatabaseModule {
             app,
             Database::class.java,
             "finacapt.db").build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(db: Database): Repository {
+        return RepositoryImpl(
+            categoryDao = db.categoryDao(),
+            incomeDao =  db.incomeDao(),
+            expenseDao = db.expenseDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideCategoryUseCases(repository: Repository): CategoryUseCases{
+        return CategoryUseCases(
+            upsertCategory = UpsertCategory(repository),
+            getAllCategories = GetAllCategories(repository)
+        )
     }
 }
