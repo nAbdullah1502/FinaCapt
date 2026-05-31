110f155a2f9e674ba9f66edc9e9b7421d7d8fd26
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-05-31 15:05:05 +0500

    AddEditCategory navigation

--- app/src/main/java/com/example/finacapt/presentation/core/screen/CategoryScreen.kt
+++ app/src/main/java/com/example/finacapt/presentation/core/screen/CategoryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.finacapt.presentation.core.screen
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -8,15 +9,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.finacapt.presentation.categories.CategoryViewModel
+import com.example.finacapt.presentation.core.AddEditCategoryActivity
 
 @Composable
 fun CategoryScreen(viewModel: CategoryViewModel){
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = { FloatingActionButton(
             onClick = {
-
+                val intent = Intent(context, AddEditCategoryActivity::class.java)
+                context.startActivity(intent)
             }
         ){ Text("+") }}
     ){ paddingValues ->
