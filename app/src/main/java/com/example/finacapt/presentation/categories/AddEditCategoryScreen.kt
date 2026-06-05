9d156f36e5a291fd6413c15fcf76f20e0b6516e8
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-06-05 18:13:20 +0500

    Color picker

--- app/src/main/java/com/example/finacapt/presentation/categories/AddEditCategoryScreen.kt
+++ app/src/main/java/com/example/finacapt/presentation/categories/AddEditCategoryScreen.kt
@@ -1,11 +1,14 @@
 package com.example.finacapt.presentation.categories
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.FloatingActionButton
@@ -18,6 +21,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.finacapt.presentation.core.util.CategoryColorProvider
+import com.example.finacapt.presentation.core.util.ColorItem
 
 @Composable
 fun AddEditCategoryScreen(
@@ -34,6 +39,17 @@ fun AddEditCategoryScreen(
             }
         },
         content = { paddingValues->
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                itemsIndexed(CategoryColorProvider.colors) { index, color ->
+                    ColorItem(
+                        color = color,
+                        selected = state.value.stateColor == index,
+                        onClick = { viewModel.onEvent(CategoryEvent.SetCategoryColor(index)) }
+                    )
+                }
+            }
             Column(
                 modifier = Modifier
                     .padding(paddingValues)
@@ -49,8 +65,12 @@ fun AddEditCategoryScreen(
                     modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                TextField(value = state.value.stateLimit, onValueChange={})
+                TextField(
+                    value = state.value.stateLimit,
+                    onValueChange={viewModel.onEvent(CategoryEvent.SetCategoryLimit(it))}
+                )
             }
+
         }
     )
 }
