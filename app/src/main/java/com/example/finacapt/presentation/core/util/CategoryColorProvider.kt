2934b0bc835b088e661b975113e02862967472c5
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-06-13 18:55:53 +0500

    Color provider colors less vivid :: semitone for the clearity of the text on the screen

--- app/src/main/java/com/example/finacapt/presentation/core/util/CategoryColorProvider.kt
+++ app/src/main/java/com/example/finacapt/presentation/core/util/CategoryColorProvider.kt
@@ -4,14 +4,14 @@ import androidx.compose.ui.graphics.Color
 
 object CategoryColorProvider {
     val colors = listOf(
-        Color(0xFF2ECC71),
-        Color(0xFF3498DB),
-        Color(0xFFE67E22),
-        Color(0xFFE74C3C),
-        Color(0xFF9B59B6),
-        Color(0xFFF1C40F),
-        Color(0xFF1ABC9C),
-        Color(0xFF34495E)
+        Color(0x802ECC71),
+        Color(0x803498DB),
+        Color(0x80E67E22),
+        Color(0x80E74C3C),
+        Color(0x809B59B6),
+        Color(0x80F1C40F),
+        Color(0x801ABC9C),
+        Color(0x8034495E)
     )
 
     val names = listOf(
