0ab7339bda9c45292a35ed45eebb919ffe86fca0
nAbdullah1502 <164726023+nAbdullah1502@users.noreply.github.com>
2026-06-13 19:38:09 +0500

    color picker alt

--- app/src/main/java/com/example/finacapt/presentation/core/screen/CategoryScreen.kt
+++ app/src/main/java/com/example/finacapt/presentation/core/screen/CategoryScreen.kt
@@ -2,21 +2,27 @@ package com.example.finacapt.presentation.core.screen
 
 import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.finacapt.presentation.categories.CategoryViewModel
 import com.example.finacapt.presentation.categories.AddEditCategoryActivity
+import com.example.finacapt.presentation.core.util.CategoryCardItem
+import com.example.finacapt.presentation.core.util.CategoryColorProvider
 
 @Composable
 fun CategoryScreen(viewModel: CategoryViewModel){
     val context = LocalContext.current
+    val categories = viewModel.getAllCategories().collectAsState(initial = emptyList())
     Scaffold(
         floatingActionButton = { FloatingActionButton(
             onClick = {
@@ -26,13 +32,22 @@ fun CategoryScreen(viewModel: CategoryViewModel){
         ){ Text("+") }}
     ){ paddingValues ->
         LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
+            columns = GridCells.Fixed(2),
             modifier = Modifier,
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = paddingValues
         ) {
-
+            items(categories.value) { category ->
+                CategoryCardItem(
+                    color = CategoryColorProvider.colors[category.colorId],
+                    title = category.title,
+                    spent = 0.0,
+                    limit = category.limit,
+                    modifier = Modifier.fillMaxSize(),
+                    onClick = {},
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/finacapt/presentation/core/util/ColorItem.kt
+++ app/src/main/java/com/example/finacapt/presentation/core/util/ColorItem.kt
@@ -1,12 +1,19 @@
 package com.example.finacapt.presentation.core.util
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,11 +22,27 @@ fun ColorItem(
     selected: Boolean,
     onClick: () -> Unit
 ) {
-
     Box(
         modifier = Modifier
-            .size(52.dp)
+            .size(
+                if (selected) 60.dp
+                else 50.dp
+            )
             .clip(CircleShape)
             .clickable(onClick = onClick)
-    )
+            .then(if (selected)
+                Modifier
+                    .border(
+                        width=2.dp,
+                        color=androidx.compose.ui.graphics.Color.Gray,
+                        shape=CircleShape)
+                    .shadow(elevation=8.dp, shape=CircleShape)
+                else Modifier
+        )
+    ) {
+        Box{
+            Canvas(modifier = Modifier.fillMaxSize()){drawCircle(color = color)}
+            if (selected) Icon(imageVector=Icons.Default.Check, contentDescription = "check" )
+        }
+    }
 }
